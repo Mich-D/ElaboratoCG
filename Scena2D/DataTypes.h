3920efe2b2adf7c4790de48fb027cda2e68f6aa3
Michele Durante <michele.durante3@studio.unibo.it>
2017-10-01 17:21:40 +0200

    Fixed sea waves, added boat prototype

--- Scena2D/DataTypes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern const double PI = 3.1415926535;
-
-typedef struct {
-	float x, y;
-} Point2D;
--- Scena2D/Main.cpp
+++ Scena2D/Main.cpp
@@ -5,51 +5,127 @@
 #include <GL\glew.h>
 #include <GL\freeglut.h>
 
-#include "DataTypes.h"
-
 #define WINDOW_WIDTH 960
-#define WINDOW_HEIGHT 540
+#define WINDOW_HEIGHT 600
+
+#define WORLD_SIZE 200.0
+
+#define PI 3.1415926535
 
 using namespace std;
 
-void init() {
+float t = 0;
+
+void init()
+{
 
 }
 
-void drawScene(void) {
-	float i;
+void drawBoat() {
+	glColor3f(1.0, 0.95, 0.9);
 
-	glClearColor(0.25, 0.5, 0.75, 1.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(-15.0, 37.5, 0.0);
+		glVertex3f(  0.0, 65.0, 0.0);
+		glVertex3f(  0.0, 37.5, 0.0);
+	glEnd();
+
+	glLineWidth(4.0);
+	glColor3f(0.55, 0.33, 0.2);
+
+	glBegin(GL_LINES);
+		glVertex3f(0.0, 35.0, 0.0);
+		glVertex3f(0.0, 65.0, 0.0);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+		glVertex3f(-15.0, 20.0, 0.0);
+		glVertex3f(-20.0, 35.0, 0.0);
+		glVertex3f( 20.0, 35.0, 0.0);
+		glVertex3f( 15.0, 20.0, 0.0);
+	glEnd();
+
+	glLineWidth(1.0);
+	glColor3f(0.0, 0.0, 0.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	glBegin(GL_POLYGON);
+		glVertex3f(-15.0, 20.0, 0.0);
+		glVertex3f(-20.0, 35.0, 0.0);
+		glVertex3f(20.0, 35.0, 0.0);
+		glVertex3f(15.0, 20.0, 0.0);
+	glEnd();
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+}
+
+void drawWave(float yValue, float height, float offset, float speed, float blueAmount)
+{
+	float x;
+
+	glColor3f(0.0, 0.0, blueAmount);
+	glBegin(GL_TRIANGLE_STRIP);
+		for (x = -(WORLD_SIZE/2); x <= (WORLD_SIZE/2); x += 5) {
+			float k = 2 * PI / 100.0;
+			float y = 5.0 * sin(k * (x + (t * speed) + offset)) + yValue;
+
+			glVertex3f(x, y - height, 0.0);
+			glVertex3f(x, y, 0.0);
+		}
+	glEnd();
+}
+
+void drawScene(void)
+{
+	glClearColor(0.2, 0.6, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.1, 0.1, 1.0);
-	glPushMatrix();
-	glTranslatef(-100.0, 0.0, 0.0);
-	glBegin(GL_LINE_STRIP);
-	for (i = 0; i < 200; i++) {
-		glVertex3f(i, sin(i), 0.0);
-	}
-	glEnd();
-	glPopMatrix();
+	drawBoat();
 
+	drawWave(-25, 50,  0, 0.50, 0.8);
+	drawWave(-50, 50, 25, 0.75, 0.6);
+	drawWave(-75, 50, 50, 1.00, 0.4);
+
+	glutSwapBuffers();
 	glFlush();
 }
 
-void resize(int w, int h) {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
+void listenKeyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	}
 }
 
-int main(int argc, char** argv) {
 
+void resize(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-(WORLD_SIZE / 2), WORLD_SIZE / 2, -(WORLD_SIZE / 2), WORLD_SIZE / 2, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void timer(int value) {
+	t++;
+	glutPostRedisplay();
+	glutTimerFunc(20, timer, 1);
+}
+
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
 
 	glutInitContextVersion(4, 2);
 	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	glutInitWindowPosition(50, 50);
 	glutCreateWindow("Scena2D");
@@ -57,7 +133,8 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(drawScene);
 	//glutMouseFunc(mouse);
 	glutReshapeFunc(resize);
-	//glutKeyboardFunc(mykeyboard);
+	glutKeyboardFunc(listenKeyboard);
+	glutTimerFunc(20, timer, 1);
 
 	glewExperimental = GL_TRUE;
 	glewInit();
